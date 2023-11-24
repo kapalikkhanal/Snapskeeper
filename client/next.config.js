c3c203f62f58ec692db4693145a97a144aad56f4
Kapalik Khanal <kapalikkhanal@gmail.com>
2023-11-24 23:39:19 +0545

    Build module failed

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build', // Specify the build output directory
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
