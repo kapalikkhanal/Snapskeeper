2007e72c050f55b3395c16db63e16bbcd9b33345
Kapalik Khanal <kapalikkhanal@gmail.com>
2023-11-24 23:23:23 +0545

    Performance error issue

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -4,6 +4,9 @@ import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }) {
+  global.performance = global.performance || {
+    now: () => new Date().getTime(),
+  };
   return <>
     <ChakraProvider>
       <Component {...pageProps} />
